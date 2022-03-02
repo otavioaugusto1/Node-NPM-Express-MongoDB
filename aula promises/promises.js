c7c20a083278356fdc4b888d3fb3bcba5dbf7fa1
Otávio Augusto Gonçalves Nogueira <otavio.a.nogueira@hotmail.com>
2022-03-02 19:42:41 -0300

    Promisses

---
+++ aula promises/promises.js
@@ -0,0 +1,18 @@
+function rand(min, max){
+  min *= 1000;
+  max *= 1000;
+  return Math.floor(Math.random() * (max - min) + min);
+}
+function esperaAi(msg, tempo){
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(msg)
+    }, tempo)  
+  })
+}
+
+esperaAi("Olá...", rand(1, 3))
+.then(resposta => {
+  console.log(resposta)
+})
+.catch()
