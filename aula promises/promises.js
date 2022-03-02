7bab73dea20e0997637058626ea20a041b6c98c8
Otávio Augusto Gonçalves Nogueira <otavio.a.nogueira@hotmail.com>
2022-03-02 20:46:27 -0300

    promises update

--- aula promises/promises.js
+++ aula promises/promises.js
@@ -16,3 +16,18 @@ esperaAi("Olá...", rand(1, 3))
   console.log(resposta)
 })
 .catch()
+
+// métodos
+// promise.all promise.race promise.resolve promise.reject
+const promises = [
+  "Primeiro valor",
+  esperaAi('Promise 1', 3000),
+  esperaAi('Promise 2', 500),
+  esperaAi('Promise 3', 1000),
+  "Outro valor"
+]
+Promise.all(promises).then(function(dados){
+  console.log(dados)
+}).catch(function(error){
+  console.log(error)
+})
